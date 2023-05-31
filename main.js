aed8a95c93598fbb43875849d5932c4a8ae2f56c
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-31 16:09:05 +0200

    milestone 1 completed

--- main.js
+++ main.js
@@ -56,11 +56,24 @@ const posts = [
     }
 ];
 
-posts.forEach(post => createPost('container', posts.image, posts.created, posts.name, posts.content, posts.media, posts.likes));
+posts.forEach(post => createPost('container', post.author.image, post.created, post.author.name, post.content, post.media, post.likes, addElement, addElementNoClass));
 
-/* createPost('container', posts.image, posts.created, posts.name, posts.content, posts.media, posts.likes); */
 
-function createPost(postsContainer, profilePicSource, dataSource, authorNameSource, textSource, imageSource, likeSource) {
+/* FUNCTIONS */
+
+/**
+ * This function dynamically creates a post inside the chosen container, populating it with information taken from an array of objects.
+ * @param {*} postsContainer The container in which the post will be created.
+ * @param {*} profilePicSource The url of the user profile pic.
+ * @param {*} dataSource The date of publication of the post.
+ * @param {*} authorNameSource The name of the author.
+ * @param {*} textSource The article content.
+ * @param {*} imageSource The article image.
+ * @param {*} likeSource The number of likes.
+ * @param {*} addElement A function that creates elements into the DOM.
+ * @param {*} addElementNoClass A function that creates elements into the DOM. It's identical to addElement, but it doesn't add a class to the element.
+ */
+function createPost(postsContainer, profilePicSource, dataSource, authorNameSource, textSource, imageSource, likeSource, addElement, addElementNoClass) {
     const postsList = document.getElementById(postsContainer);
     const post = addElement('div', '', 'post', postsList);
     const postHeader = addElement('div', '', 'post__header', post);
@@ -71,18 +84,23 @@ function createPost(postsContainer, profilePicSource, dataSource, authorNameSour
     const postMetaAuthor = addElement('div', authorNameSource, 'post-meta__author', postMetaData);
     const postMetaTime = addElement('div', dataSource, 'post-meta__time', postMetaData);
     const postText = addElement('div', textSource, 'post__text', post);
-    const postImage = addElement('img', '', 'post-image', post).src = imageSource;
+    const postImage = addElement('div', '', 'post__image', post);
+    const postImageElement = addElementNoClass('img', '', postImage).src = imageSource;
     const postFooter = addElement('div', '', 'post__footer', post);
     const likes = addElement('div', '', 'likes', postFooter);
     likes.classList.add('js-likes');
-    const likesCta = addElement('div', `<a class="like-button  js-like-button" href="#" data-postid="1"><i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i><span class="like-button__label">Mi Piace</span></a>`, 'likes__cta', likes);
-    const likesCounter = addElement('div', `Piace a <b id="like-counter-1" class="js-likes-counter">${likeSource}</b> persone`, 'likes__counter', likes);
+    const likesCta = addElement('div',
+    `<a class="like-button  js-like-button" href="#" data-postid="1">
+    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+    <span class="like-button__label">Mi Piace</span></a>`,
+    'likes__cta', likes);
+    const likesCounter = addElement('div',
+    `Piace a <b id="like-counter-1" class="js-likes-counter">${likeSource}</b> persone`,
+    'likes__counter', likes);
 }
 
-/* FUNCTIONS */
-
 /**
- * This function creates an html element of the chosen type and class, with the chosen text, and adds it at the end of the selected container.
+ * This function creates into the DOM a new element of the chosen type and class, with the chosen text, and adds it at the end of the selected container.
  * @param {*} type The type of html element that will be created. 
  * @param {*} innerText The text that the created element will have inside.
  * @param {*} class The name of the class that will be added to the created element.
@@ -92,6 +110,19 @@ function addElement(type, innerText, elementClass, container) {
     const element = document.createElement(type);
     element.innerHTML = innerText;
     element.classList.add(elementClass);
+    container.append(element);
+    return element;
+}
+
+/**
+ * This function creates an html element of the chosen type, with the chosen text, and adds it at the end of the selected container.
+ * @param {*} type The type of html element that will be created. 
+ * @param {*} innerText The text that the created element will have inside.
+ * @param {*} container The container at the end of which the element will be added.
+ */
+function addElementNoClass(type, innerText, container) {
+    const element = document.createElement(type);
+    element.innerHTML = innerText;
     container.append(element);
     return element;
 }
