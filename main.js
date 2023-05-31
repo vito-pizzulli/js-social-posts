fee737aed88f962234cb071c8109e1d0ceb6a7bb
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-31 15:19:54 +0200

    creating the createPost function, all const added

--- main.js
+++ main.js
@@ -55,3 +55,41 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+createPost('container');
+
+function createPost(postsContainer, profilePicSource, dataSource, authorSource, textSource, imageSource, likeSource) {
+    const postsList = document.getElementById(postsContainer);
+    const post = addElement('div', '', 'post', postsList);
+    const postHeader = addElement('div', '', 'post__header', post);
+    const postMeta = addElement('div', '', 'post-meta', postHeader);
+    const postMetaIcon = addElement('div', '', 'post-meta__icon', postMeta);
+    const profilePic = addElement('img', '', 'profile-pic', postMetaIcon).src(profilePicSource);
+    const postMetaData = addElement('div', dataSource, 'post-meta__data', postMeta);
+    const postMetaAuthor = addElement('div', authorSource, 'post-meta__author', postMetaData);
+    const postMetaTime = addElement('div', '', 'post-meta__time', postMetaData);
+    const postText = addElement('div', textSource, 'post__text', post);
+    const postImage = addElement('img', '', '', post).src(imageSource);
+    const postFooter = addElement('div', '', 'post__footer', post);
+    const likes = addElement('div', '', 'likes', postFooter);
+    likes.classList.add('js-likes');
+    const likesCta = addElement('div', `<a class="like-button  js-like-button" href="#" data-postid="1"><i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i><span class="like-button__label">Mi Piace</span></a>`, 'likes__cta', likes);
+    const likesCounter = addElement('div', `Piace a <b id="like-counter-1" class="js-likes-counter">${likeSource}</b> persone`, 'likes__counter', likes);
+}
+
+/* FUNCTIONS */
+
+/**
+ * This function creates an html element of the chosen type and class, with the chosen text, and adds it at the end of the selected container.
+ * @param {*} type The type of html element that will be created. 
+ * @param {*} innerText The text that the created element will have inside.
+ * @param {*} class The name of the class that will be added to the created element.
+ * @param {*} container The container at the end of which the element will be added.
+ */
+function addElement(type, innerText, elementClass, container) {
+    const element = document.createElement(type);
+    element.innerHTML = innerText;
+    element.classList.add(elementClass);
+    container.append(element);
+    return element;
+}
